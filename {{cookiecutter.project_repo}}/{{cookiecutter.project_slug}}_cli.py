4dea0eb261a3918b2a64867f1b0a50fbde2dc9f4
Thibault Ducret <thibault.ducret@gmail.com>
2018-09-02 23:29:53 +0200

    Updated with more examples and comments

--- {{cookiecutter.project_repo}}/{{cookiecutter.project_slug}}_cli.py
+++ {{cookiecutter.project_repo}}/{{cookiecutter.project_slug}}_cli.py
@@ -10,20 +10,35 @@ import {{cookiecutter.project_slug}}
 
 @click.command()
 @click.option(
-    '--param1', '-p',
+    '--paramwithenvvar', '-p',
     envvar="PARAM1",
     type=str,
     help='example string param (or set the env var PARAM1)',
 )
 @click.option(
-    '--defaultparam2', '-d',
+    '--paramwithdefaultvalue', '-d',
     type=str,
     help='example param with default value',
     default='fr'
 )
-def main(param1, defaultparam2):
-    """ Example main """
-
+@click.option(
+    '--requiredparam', '-f',
+    type=str,
+    help='param required',
+    required=True,
+)
+@click.option(
+    '--flagparam',
+    is_flag=True,
+    help='flag param (True if set, False if not)',
+)
+def main(paramwithenvvar, paramwithdefaultvalue, requiredparam, flagparam):
+    """
+    !!! TO BE UPDATED !!!
+    Description of the CLI tool,
+    will be used in the --help
+    """
+    print()
 
 if __name__ == "__main__":
     main()
